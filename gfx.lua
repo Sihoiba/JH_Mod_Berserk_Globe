6677be0e050f5836d7867a18f1815993a8ece18e
Sihoiba <dominic.decesare+github@gmail.com>
2024-03-06 20:32:02 +0000

    Add a buff effect as has been done for the class skills in 1.8

--- gfx.lua
+++ gfx.lua
@@ -12,3 +12,32 @@ register_gfx_blueprint "berserk_globe"
 		color       = vec4(1,0.2,1,1),
 	},
 }
+
+register_gfx_blueprint "buff_berserk"
+{
+	equip = {},
+	persist = true,
+	point_generator = {
+		type    = "cylinder",
+		extents = vec3(0.2,0.9,0.0),
+	},
+	particle = {
+		material       = "data/texture/particles/shapes_01/blick_01",
+		group_id       = "pgroup_fx",
+		orientation    = PS_ORIENTED,
+		destroy_owner  = true,
+	},
+	particle_emitter = {
+		rate     = 96,
+		size     = vec2(0.05,0.1),
+		velocity = 0.1,
+		lifetime = 0.5,
+		color    = vec4(1.0,0.1,0.1,0.5),
+	},
+	particle_transform = {
+		force = vec3(0,3,0),
+	},
+	particle_fade = {
+		fade_out = 0.5,
+	},
+}
